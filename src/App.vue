bdb6e34f5630ac06e9a7f0e24517508dbd3651be
= <=>
2022-04-14 08:43:28 +0530

    text binding

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script >
+  export default{
+    name:"App",
+    data(){
+      return{
+        name:"abc"
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>hello, {{name}}</h1>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
