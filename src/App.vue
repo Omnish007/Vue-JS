0a8b8af09e494904ad492ec25eea661858310504
= <=>
2022-04-14 22:42:51 +0530

    html binding

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
       return{
         greet:"hello",
         name:"abc",
-        age:30
+        age:"<b>30</b>",
+        hack:`<a href='#' onClick="alert('you have been hacked !')"> Win a prize!</a>`
       }
     }
   }
@@ -14,11 +15,18 @@
 
 
 <template>
-  <h1>{{greet}}, {{name}}</h1>
+
+  <!-- both render as plain text  -->
+  <div>{{age}}</div>
   <div v-text="age"></div>
 
-  <!-- gives error -->
-  <!-- <div v-text="age">hello</div> -->
+  <!-- rendered as html  -->
+  <div v-html="age"></div>
+
+  <!-- don't use other non trusted api data as v-html because it can cause cross-site scripting attack -->
+  <!-- ex....  -->
+  <div  v-html="hack"></div>
+
 </template>
 
 <style>
