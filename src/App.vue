9deffc7be663441db8a72655324bc9ea6054d29d
= <=>
2022-04-14 23:15:30 +0530

    classes binding

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,45 @@
 <script >
-  export default{
-    name:"App",
-    data(){
-      return{
-          headingId:'heading',
-          isDisabled: true
-      }
-    }
-  }
+export default {
+  name: "App",
+  data() {
+    return {
+      status: "danger",
+      isPromoted: false,
+      isSoldOut: true,
+    };
+  },
+};
 </script>
 
 
 
 <template>
-    <h1 v-bind:id="headingId">id of this element is binding with v-bind:id</h1>
+  <h1 class="underline">Underline text using static class</h1>
+  <h1 v-bind:class="status">Change text color using dynamic class</h1>
 
-    <button v-bind:disabled="isDisabled">Hello</button>
+  <!-- we can use binded class and static class also -->
+  <h1 v-bind:class="status" class="underline">
+    Use of static and dynamic class
+  </h1>
+
+  <!-- conditional binding  -->
+  <h1 v-bind:class="isPromoted && 'promotede'">conditional binding</h1>
+
+  <!-- ternary operators -->
+  <h1 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Ternary operator</h1>
+
+  <!-- binding with  array of classes -->
+  <h1 v-bind:class="['sold-out', 'promoted']">Array of classes</h1>
+
+  <!-- binding with  object of classes -->
+  <h1
+    v-bind:class="{
+      'sold-out': isPromoted,
+      new: isSoldOut,
+    }"
+  >
+    Obj of classes
+  </h1>
 </template>
 
 <style>
@@ -26,5 +50,25 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.danger {
+  color: red;
+}
+
+.promoted {
+  font-style: italic;
+}
+
+.new {
+  color: green;
+}
+
+.sold-out {
+  color: red;
 }
 </style>
