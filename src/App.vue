75fce15c95fba8c6997746779c1103270a3385b9
= <=>
2022-04-14 22:48:01 +0530

    attributes binding

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,8 @@
     name:"App",
     data(){
       return{
-        greet:"hello",
-        name:"abc",
-        age:"<b>30</b>",
-        hack:`<a href='#' onClick="alert('you have been hacked !')"> Win a prize!</a>`
+          headingId:'heading',
+          isDisabled: true
       }
     }
   }
@@ -15,18 +13,9 @@
 
 
 <template>
+    <h1 v-bind:id="headingId">id of this element is binding with v-bind:id</h1>
 
-  <!-- both render as plain text  -->
-  <div>{{age}}</div>
-  <div v-text="age"></div>
-
-  <!-- rendered as html  -->
-  <div v-html="age"></div>
-
-  <!-- don't use other non trusted api data as v-html because it can cause cross-site scripting attack -->
-  <!-- ex....  -->
-  <div  v-html="hack"></div>
-
+    <button v-bind:disabled="isDisabled">Hello</button>
 </template>
 
 <style>
