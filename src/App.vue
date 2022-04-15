dc42773a23d30007a7181ef3e7b3f51b176c4e8f
= <=>
2022-04-16 01:40:01 +0530

    binding styles

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,15 @@ export default {
   name: "App",
   data() {
     return {
-      status: "danger",
-      isPromoted: false,
-      isSoldOut: true,
+      highlight: "purple",
+      headerStyles: {
+        color: "orange",
+        fontSize: "30px",
+        textDecoration: "underline",
+      },
+      bgStyle: {
+        "background-color": "red",
+      },
     };
   },
 };
@@ -14,32 +20,14 @@ export default {
 
 
 <template>
-  <h1 class="underline">Underline text using static class</h1>
-  <h1 v-bind:class="status">Change text color using dynamic class</h1>
+  <!-- it also allows camel case in styles like "font-size" and fontSize -->
+  <h1 v-bind:style="{ color: highlight, fontSize: '50px' }">Initial styles</h1>
 
-  <!-- we can use binded class and static class also -->
-  <h1 v-bind:class="status" class="underline">
-    Use of static and dynamic class
-  </h1>
+  <!-- pass classes object -->
+  <h1 v-bind:style="headerStyles">Initial styles</h1>
 
-  <!-- conditional binding  -->
-  <h1 v-bind:class="isPromoted && 'promotede'">conditional binding</h1>
-
-  <!-- ternary operators -->
-  <h1 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Ternary operator</h1>
-
-  <!-- binding with  array of classes -->
-  <h1 v-bind:class="['sold-out', 'promoted']">Array of classes</h1>
-
-  <!-- binding with  object of classes -->
-  <h1
-    v-bind:class="{
-      'sold-out': isPromoted,
-      new: isSoldOut,
-    }"
-  >
-    Obj of classes
-  </h1>
+  <!-- pass classes array -->
+  <h1 v-bind:style="[headerStyles, bgStyle]">Initial styles</h1>
 </template>
 
 <style>
@@ -50,25 +38,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.underline {
-  text-decoration: underline;
-}
-
-.danger {
-  color: red;
-}
-
-.promoted {
-  font-style: italic;
-}
-
-.new {
-  color: green;
-}
-
-.sold-out {
-  color: red;
 }
 </style>
