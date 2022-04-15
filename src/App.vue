29f8c75368518ff91a74005de7323baf23bcc46b
= <=>
2022-04-16 02:04:41 +0530

    conditional rendering directives

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,9 @@ export default {
   name: "App",
   data() {
     return {
-      highlight: "purple",
-      headerStyles: {
-        color: "orange",
-        fontSize: "30px",
-        textDecoration: "underline",
-      },
-      bgStyle: {
-        "background-color": "red",
-      },
+      num: "27",
+      display: true,
+      showElement: true,
     };
   },
 };
@@ -20,14 +14,32 @@ export default {
 
 
 <template>
-  <!-- it also allows camel case in styles like "font-size" and fontSize -->
-  <h1 :style="{ color: highlight, fontSize: '50px' }">Initial styles</h1>
+  <!-- v-if  -->
+  <h1 v-if="num === 0">Number is 0</h1>
+  <h1 v-if="num === 5">Number is 5</h1>
+  <hr />
 
-  <!-- pass classes object -->
-  <h1 :style="headerStyles">Initial styles</h1>
+  <!-- v-else -->
+  <h1 v-if="num === 5">Number is 5</h1>
+  <h2 v-else>Number is not equal to 5</h2>
+  <hr />
 
-  <!-- pass classes array -->
-  <h1 :style="[headerStyles, bgStyle]">Initial styles</h1>
+  <!-- v-else-if -->
+  <h1 v-if="num === 0">Number is 0</h1>
+  <h2 v-else-if="num < 0">Number is negative</h2>
+  <h2 v-else-if="num > 0">Number is positive</h2>
+  <h2 v-else>Not a number</h2>
+
+  <!-- we can pass div also but div is rendered in dom template is not -->
+  <template v-if="display">
+    <h1>Vue</h1>
+    <h1>Js</h1>
+    <h1>Front-end</h1>
+  </template>
+
+  <!-- diffrence between v-if and v-show if it return false then v-if element is not present in dom but v-show element is present in dom with display none propertie -->
+  <!-- v-show -->
+  <h2 v-show="showElement">Using v-show</h2>
 </template>
 
 <style>
