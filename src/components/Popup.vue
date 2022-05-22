f5d3a4a96c845fffb76f7989bcc3b1f22e19245d
omnish.inexture <omnish.inexture@gmail.com>
2022-05-22 12:57:29 +0530

    Validating Emitted Events

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
         <h2>Popup</h2>
+        <input type="text" v-model="name" />
         <!-- $emit(eventname ,data which we want to pass to parent) -->
-        <button @click="$emit('close', 'omnish')">Close Popup</button>
+        <button @click="$emit('close', name)">Close Popup</button>
     </div>
 </template>
 
@@ -11,6 +12,18 @@ export default {
     name: "Popup",
     // array of events that the componrt can emit to the parent component
     // we can give any name
-    emits: ["close"],
+    emits: {
+        close: (name) => {
+            if (!name) {
+                return false;
+            }
+            return true;
+        },
+    },
+    data() {
+        return {
+            name: "",
+        };
+    },
 };
 </script>
